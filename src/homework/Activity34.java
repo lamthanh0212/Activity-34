3c780d248587c10162431003d9dcf32e410c5d77
unknown <lamnguyen02122004@gmail.com>
2021-06-11 10:44:10 +0700

    Add project

--- src/homework/Activity34.java
+++ src/homework/Activity34.java
@@ -6,7 +6,7 @@ public class Activity34 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         float socuoi= scanner.nextInt();
-        int sum = 0 ;
+        float sum = 0 ;
         float avg ;
         for (int i = 1 ; i <= socuoi ; i=i+1 ){
             sum = sum +  i ;
